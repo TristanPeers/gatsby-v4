5ea22950478c34edeaadcee77c3d8c3146d49736
Tristan Peers <tristan.peers@engineering.digital.dwp.gov.uk>
2022-03-16 10:14:25 +0000

    Add current elasticsearh plugin to help illustrate same issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,19 @@ module.exports = {
           })),
       },
     },
+    {
+      resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
+      options: {
+        // Fields to index
+        fields: [`path`],
+        // How to resolve each field`s value for a supported node type
+        resolvers: {
+          // For any node of type SitePage, list how to resolve the fields` values
+          SitePage: {
+            path: node => node.path,
+          },
+        },
+      },
+    },
   ],
 };
